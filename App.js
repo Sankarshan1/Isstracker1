5d86e163b5f097fcd5b418590c69f53663869da5
Sankarshan <Sankarshan@gmail>
2021-06-21 19:54:49 +0530

    Isstracker1

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screens/homeScreen';
+import ISSLocationScreen from './Screens/ISSLocationScreen';
+import MeteorScreen from './Screens/meteorScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="ISS" component={ISSLocationScreen} />
+      <Stack.Screen name="Meteor" component={MeteorScreen} />
+      </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
